536310248003665b4adb8a9cc662e3c7c83a2467
afucher <arthurfucher@gmail.com>
2015-03-10 22:23:33 -0300

    add Download

--- src/main/java/filestorage/FileStorageController.java
+++ src/main/java/filestorage/FileStorageController.java
@@ -1,9 +1,16 @@
 package filestorage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +43,27 @@ public class FileStorageController {
     
     private File convertToFile(GridFSDBFile file){
     	return new File(file.getFilename());
+    }
+    
+    @RequestMapping(value="/file", method=RequestMethod.GET)
+    public void handleFileDownload(@RequestParam("name") String name, HttpServletRequest request, HttpServletResponse response){
+    	GridFSDBFile file = gridFsTemplate.findOne(new Query().addCriteria(Criteria.where("filename").is(name)));
+        if (file != null) {
+            try {
+                response.setContentType(file.getContentType());
+                response.setContentLength((new Long(file.getLength()).intValue()));
+                response.setHeader("content-Disposition", "attachment; filename=" + file.getFilename());// "attachment;filename=test.xls"
+                // copy it to response's OutputStream
+                IOUtils.copyLarge(file.getInputStream(), response.getOutputStream());
+            } catch (IOException ex) {
+                //_logger.info("Error writing file to output stream. Filename was '" + id + "'");
+                //throw new RuntimeException("IOError writing file to output stream");
+            }
+        }
+    	
+    	
+    	
+    	//return (MultipartFile) file;
     }
     
     
